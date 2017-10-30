c1332eac74f3f83fe5ef0b76b882d94be8f25107
Melicias <melicias1999@gmail.com>
2017-10-30 15:55:52 +0000

    Remove footer and menu html
    Removed the menu bar and footer .html and added everything to the index.html

--- web/js/my.js
+++ web/js/my.js
@@ -1,6 +1,13 @@
 $(function(){
     $("#footerhtml").load("footer.html");
-    $("#menuhtml").load("menu.html"); 
+    $("#menuhtml").load("menu.html");
+    //$("#contentor").load("inicio_menu.html");
+    
+    $("#butaoIniciarJogo").click(function(){
+        alert("AAA");
+       // $("#contentor").load("jogo.html");
+        
+    }); 
 });
 
 
