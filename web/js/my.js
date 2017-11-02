dcaf37fe8c1bd522156b4f1007b2fea9b27f5cf3
Melicias <melicias1999@gmail.com>
2017-11-02 18:36:36 +0000

    Fixed Bugs
    Changed javascript file and fixed index

--- web/js/my.js
+++ web/js/my.js
@@ -1,13 +1,17 @@
 $(function(){
-    $("#footerhtml").load("footer.html");
-    $("#menuhtml").load("menu.html");
     //$("#contentor").load("inicio_menu.html");
     
+    //Butao adicionar
+    $("#contentor").html("<div id='div_butoes_inicio_menu'>\n\
+                            <button id='butaoIniciarJogo' type='button' style='display: block; margin:15px; width:500px ; height: 250px; font-size: 60px;' class='btn btn-primary btn-lg' >Iniciar Jogo</button>\n\
+                         </div>");
+    
     $("#butaoIniciarJogo").click(function(){
-        alert("AAA");
-       // $("#contentor").load("jogo.html");
+        $("#contentor").html("<div id='div_escolher_dificuldade'>\n\
+                             </div>")
         
-    }); 
+    });   
+    
 });
 
 
