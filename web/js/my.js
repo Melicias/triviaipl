5906b9bae34bdbc31e5955e56654c98b850cd6d7
Melicias <melicias1999@gmail.com>
2017-11-05 01:25:07 +0000

    Bug in JS
    Need to use this to work
    $(document).on("click"

--- web/js/my.js
+++ web/js/my.js
@@ -1,16 +1,53 @@
-$(function(){
+/* LINK
+ * https://opentdb.com/api.php?amount=10&category=18&difficulty=easy
+ * 
+ * A dificuldade pode ser easy ou medium
+ * categoria 9 - Conhecimento geral
+ * categoria 15 - video jogos
+ * categoria 18 - computadores
+ * 
+ */
+
+$(document).ready(function(){
     //$("#contentor").load("inicio_menu.html");
+    
+    var dificuldade;
+    var categoria;
+    
     
     //Butao adicionar
     $("#contentor").html("<div id='div_butoes_inicio_menu'>\n\
-                            <button id='butaoIniciarJogo' type='button' style='display: block; margin:15px; width:500px ; height: 250px; font-size: 60px;' class='btn btn-primary btn-lg' >Iniciar Jogo</button>\n\
+                            <button id='butaoIniciarJogo' type='button' style='display: block; margin:15px; width:250px ; height: 125px; font-size: 35px;' class='btn btn-primary btn-lg' >Iniciar Jogo</button>\n\
                          </div>");
     
     $("#butaoIniciarJogo").click(function(){
         $("#contentor").html("<div id='div_escolher_dificuldade'>\n\
-                             </div>")
-        
-    });   
+                                <h2>Escolha a dificuldade:</h2>\n\
+                                <button id='butaoDificuldadeFacil' type='button' style=' margin:30px; width:200px ; height: 100px; font-size: 35px;' class='btn btn-primary'>Fácil</button>\n\
+                                <button id='butaoDificuldadeMedia' type='button' style=' margin:30px; width:200px ; height: 100px; font-size: 35px;' class='btn btn-primary'>Média</button>\n\
+                            </div>");  
+    });
+    
+    
+    $(document).on("click","#butaoDificuldadeFacil",function(){
+        dificuldade(1);
+    });
+    
+    $(document).on("click","#butaoDificuldadeMedia",function(){
+        dificuldade(2);
+    });
+    
+    
+    function dificuldade(id){
+        if(id == 1){
+            alert("1");
+            return 0;
+        } else{
+            alert("2");
+        }
+        return 1;
+    }
+    
     
 });
 
