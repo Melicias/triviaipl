d705724eedb9bf2523ef5b5719947063e45aeade
Melicias <melicias1999@gmail.com>
2017-11-12 22:34:40 +0000

    js file changed
    added js code

--- web/js/my.js
+++ web/js/my.js
@@ -2,16 +2,16 @@
  * https://opentdb.com/api.php?amount=10&category=18&difficulty=easy
  * 
  * A dificuldade pode ser easy ou medium
- * categoria 9 - Conhecimento geral
+ * categoria 21 - Desporto
  * categoria 15 - video jogos
- * categoria 18 - computadores
+ * categoria 18 - computadores/informatica
  * 
  */
 
 $(document).ready(function(){
     //$("#contentor").load("inicio_menu.html");
     
-    var dificuldade;
+    var dificuldade; //dificuldade facil(easy) / dificuldade media(medium)
     var categoria;
     
     
@@ -30,23 +30,66 @@ $(document).ready(function(){
     
     
     $(document).on("click","#butaoDificuldadeFacil",function(){
-        dificuldade(1);
+        escolherDificuldade("easy");
     });
     
     $(document).on("click","#butaoDificuldadeMedia",function(){
-        dificuldade(2);
+        escolherDificuldade("medium");
     });
     
     
-    function dificuldade(id){
-        if(id == 1){
-            alert("1");
-            return 0;
-        } else{
-            alert("2");
-        }
-        return 1;
+    function escolherDificuldade(id){
+        dificuldade = id;
+        $("#contentor").html("<div id='div_escolher_categoria'>\n\
+                                <button type='button' id='botaoinformatica' style=' margin:30px; width:200px ; height: 100px; font-size: 25px;' class='btn btn-primary'>Informática</button>\n\
+                                <button type='button' id='botaojogos' style=' margin:30px; width:200px ; height: 100px; font-size: 25px;' class='btn btn-primary'>Video-Jogos</button>\n\
+                                <button type='button' id='botaodesporto' style=' margin:30px; width:200px ; height: 100px; font-size: 25px;' class='btn btn-primary'>Desporto</button>\n\
+                                <button type='button' id='botaoRandom' style=' margin:30px; width:200px ; height: 100px; font-size: 25px;' class='btn btn-primary'>Random</button>\n\
+                              </div>"); 
     }
+    
+    $(document).on("click","#botaoinformatica",function(){
+        escolherCategoria(18);
+    });
+    
+    $(document).on("click","#botaojogos",function(){
+        escolherCategoria(15);
+    });
+    
+    $(document).on("click","#botaodesporto",function(){
+        escolherCategoria(21);
+    });
+    
+    $(document).on("click","#botaoRandom",function(){
+        escolherCategoria(-1);
+    });
+    
+    
+    function escolherCategoria(cat){
+        categoria = cat;
+        if(cat == -1){
+            var random = Math.floor((Math.random() * 3) + 1);
+            switch(random){
+                case 1:
+                    categoria = 18;
+                    break;
+                case 2:
+                    categoria = 15;
+                    break;
+                case 3:
+                    categoria = 21;
+                    break;
+                default:
+                    categoria = 18;
+            }
+        }
+         generarLinkAPI();
+    }
+    
+    function generarLinkAPI(){
+        var link = "https://opentdb.com/api.php?amount=10&category=" + categoria + "&difficulty="+ dificuldade;
+    }
+    
     //https://www.w3schools.com/html/html5_webstorage.asp
     
 });
